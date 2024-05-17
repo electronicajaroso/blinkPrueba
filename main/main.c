6ea090d9108f7f1c5af463d77fc1781651e861d9
electronicajaroso <electronicajaroso@gmail.com>
2024-05-17 13:37:31 +0200

    blink funicional 500ms con delay

--- main/main.c
+++ main/main.c
@@ -17,9 +17,32 @@ struct led
 //declaramos led1
 struct led led1 = {2, 0};
 
+//funciones a utilizar
+esp_err_t initLed(void);
+esp_err_t blinkLed(void);
 
 
 void app_main(void)
 {
+    initLed();
+    while(true)
+    {
+        blinkLed();
+        printf("estado led %d", led1.valorLed);
+    }
+}
+
+
+esp_err_t initLed(void)
+{
+    gpio_set_direction(led1.pin, GPIO_MODE_OUTPUT);
+    return ESP_OK;
+}
+esp_err_t blinkLed(void)
+{
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+    led1.valorLed = !led1.valorLed;
+    gpio_set_level(led1.pin, led1.valorLed);
+    return ESP_OK;
 
 }
