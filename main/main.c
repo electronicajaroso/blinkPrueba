1d48a5bace4801034c1c24124832a205b107639e
electronicajaroso <electronicajaroso@gmail.com>
2024-05-17 13:24:49 +0200

    Librerias y dato tipo led creados

--- main/main.c
+++ main/main.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+//incluir librerias gestión gpio
+#include "driver/gpio.h"
+
+//incluir libreria gestión de tiempos sincronos
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+//datos control led
+struct led
+{
+    int pin;
+    int valorLed;
+};
+
+//declaramos led1
+struct led led1 = {2, 0};
+
+
+
 void app_main(void)
 {
 
